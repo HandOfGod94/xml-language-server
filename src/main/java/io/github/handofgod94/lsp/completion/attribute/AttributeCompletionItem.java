c141c0a21ed0879a66b563e0ffaf08f58a64b961
Gahan Rakholia <gahan.rakholia@jda.com>
2018-11-08 23:49:59 +0530

    feat(autocomplete): attr doc for autocomplete
    Type informations for attribute will be displayed in the
    completions suggestions. More information such as max occurs or
    complex type defintions will be handled separately.

--- src/main/java/io/github/handofgod94/lsp/completion/attribute/AttributeCompletionItem.java
+++ src/main/java/io/github/handofgod94/lsp/completion/attribute/AttributeCompletionItem.java
@@ -1,20 +1,21 @@
 package io.github.handofgod94.lsp.completion.attribute;
 
+import org.apache.xerces.xs.XSSimpleTypeDefinition;
 import org.eclipse.lsp4j.CompletionItem;
 import org.eclipse.lsp4j.CompletionItemKind;
 import org.eclipse.lsp4j.InsertTextFormat;
+import org.eclipse.lsp4j.MarkupContent;
+import org.eclipse.lsp4j.MarkupKind;
 
 public class AttributeCompletionItem {
   private String name;
-  private String namespace;
-  private String type;
+  private XSSimpleTypeDefinition type;
   private int maxOccurs;
   private int minOccurs;
   private boolean isRequired;
 
-  public AttributeCompletionItem(String name, String namespace, String type, int maxOccurs, int minOccurs, boolean isRequired) {
+  public AttributeCompletionItem(String name, XSSimpleTypeDefinition type, int maxOccurs, int minOccurs, boolean isRequired) {
     this.name = name;
-    this.namespace = namespace;
     this.type = type;
     this.maxOccurs = maxOccurs;
     this.minOccurs = minOccurs;
@@ -29,19 +30,11 @@ public class AttributeCompletionItem {
     this.name = name;
   }
 
-  public String getNamespace() {
-    return namespace;
-  }
-
-  public void setNamespace(String namespace) {
-    this.namespace = namespace;
-  }
-
-  public String getType() {
+  public XSSimpleTypeDefinition getType() {
     return type;
   }
 
-  public void setType(String type) {
+  public void setType(XSSimpleTypeDefinition type) {
     this.type = type;
   }
 
@@ -73,10 +66,22 @@ public class AttributeCompletionItem {
     // Initialize AttributeCompletionItem
     CompletionItem info = new CompletionItem();
     info.setLabel(name);
-    info.setDetail(namespace);
+    info.setDetail(type.getName());
+    info.setDocumentation(buildDoc());
     info.setInsertTextFormat(InsertTextFormat.Snippet);
     info.setInsertText(name + "=\"$1\"");
     info.setKind(CompletionItemKind.Property);
     return info;
+  }
+
+  private MarkupContent buildDoc() {
+    MarkupContent doc = new MarkupContent();
+    doc.setKind(MarkupKind.MARKDOWN);
+    String typeDoc = String.format("**Type**: %s  \n", type.getName());
+    String namespaceDoc = String.format("**Namespace**: %s  \n", type.getNamespace());
+    String requiredDoc = String.format("**Required**: %b  \n", isRequired);
+    doc.setValue(typeDoc + namespaceDoc + requiredDoc);
+
+    return doc;
   }
 }
