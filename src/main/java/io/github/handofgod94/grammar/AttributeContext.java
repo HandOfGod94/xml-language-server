19824131ecafee1428f963257e29f8ecf4c0b646
Gahan Rakholia <gahan.rakholia@jda.com>
2018-12-02 23:03:38 +0530

    refactor: move common features to LanguageContext

--- src/main/java/io/github/handofgod94/grammar/AttributeContext.java
+++ src/main/java/io/github/handofgod94/grammar/AttributeContext.java
@@ -2,9 +2,6 @@ package io.github.handofgod94.grammar;
 
 import io.github.handofgod94.grammar.visitors.Visitor;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import org.eclipse.lsp4j.CompletionList;
 import org.eclipse.lsp4j.Position;
 
 public class AttributeContext extends LanguageContext implements GrammarElement {
@@ -14,14 +11,6 @@ public class AttributeContext extends LanguageContext implements GrammarElement 
   public static final int ENTITY_OTHER_ATTRIBUTE_NAME_LOCALNAME_XML = 3;
 
   private final String regEx = "(?:^|\\s+)(?:([-\\w.]+)(:))?([-\\w.:]+)\\s*=";
-  private final List<Integer> groupsToMatch;
-
-  private List<CompletionList> completionList;
-  private boolean isMatched = false;
-  private boolean isInScope = false;
-  private Map<Integer, String> capturedGroups;
-  private int matchStart = -1;
-  private int matchEnd = -1;
 
   AttributeContext(String line, Position position) {
     super(line, position);
@@ -42,67 +31,15 @@ public class AttributeContext extends LanguageContext implements GrammarElement 
     return regEx;
   }
 
-  public List<Integer> getGroupsToMatch() {
-    return groupsToMatch;
-  }
-
-  public List<CompletionList> getCompletionList() {
-    return completionList;
-  }
-
-  public void setCompletionList(List<CompletionList> completionList) {
-    this.completionList = completionList;
-  }
-
-  public boolean isMatched() {
-    return isMatched;
-  }
-
-  public boolean isInScope() {
-    return isInScope;
-  }
-
-  public void setInScope(boolean inScope) {
-    isInScope = inScope;
-  }
-
-  public int getMatchStart() {
-    return matchStart;
-  }
-
-  public void setMatchStart(int matchStart) {
-    this.matchStart = matchStart;
-  }
-
-  public int getMatchEnd() {
-    return matchEnd;
-  }
-
-  public void setMatchEnd(int matchEnd) {
-    this.matchEnd = matchEnd;
-  }
-
-  public void setMatched(boolean matched) {
-    isMatched = matched;
-  }
-
-  public Map<Integer, String> getCapturedGroups() {
-    return capturedGroups;
-  }
-
-  public void setCapturedGroups(Map<Integer, String> capturedGroups) {
-    this.capturedGroups = capturedGroups;
-  }
-
   @Override
   public String toString() {
     return "AttributeContext{" +
       "regEx='" + regEx + '\'' +
-      ", groupsToMatch=" + groupsToMatch +
-      ", completionList=" + completionList +
+      ", line='" + line + '\'' +
+      ", position=" + position +
+      ", capturedGroups=" + capturedGroups +
       ", isMatched=" + isMatched +
       ", isInScope=" + isInScope +
-      ", capturedGroups=" + capturedGroups +
       ", matchStart=" + matchStart +
       ", matchEnd=" + matchEnd +
       '}';
--- src/main/java/io/github/handofgod94/grammar/LanguageContext.java
+++ src/main/java/io/github/handofgod94/grammar/LanguageContext.java
@@ -1,11 +1,20 @@
 package io.github.handofgod94.grammar;
 
+import java.util.List;
+import java.util.Map;
 import org.eclipse.lsp4j.Position;
 
 public abstract class LanguageContext {
 
   protected final String line;
   protected final Position position;
+
+  protected List<Integer> groupsToMatch;
+  protected Map<Integer, String> capturedGroups;
+  protected boolean isMatched = false;
+  protected boolean isInScope = false;
+  protected int matchStart = -1;
+  protected int matchEnd = -1;
 
   LanguageContext(String line, Position position) {
     this.line = line;
@@ -17,5 +26,49 @@ public abstract class LanguageContext {
   }
   public Position getPosition() {
     return position;
+  }
+
+  public List<Integer> getGroupsToMatch() {
+    return groupsToMatch;
+  }
+
+  public Map<Integer, String> getCapturedGroups() {
+    return capturedGroups;
+  }
+
+  public void setCapturedGroups(Map<Integer, String> capturedGroups) {
+    this.capturedGroups = capturedGroups;
+  }
+
+  public boolean isMatched() {
+    return isMatched;
+  }
+
+  public void setMatched(boolean matched) {
+    isMatched = matched;
+  }
+
+  public boolean isInScope() {
+    return isInScope;
+  }
+
+  public void setInScope(boolean inScope) {
+    isInScope = inScope;
+  }
+
+  public int getMatchStart() {
+    return matchStart;
+  }
+
+  public void setMatchStart(int matchStart) {
+    this.matchStart = matchStart;
+  }
+
+  public int getMatchEnd() {
+    return matchEnd;
+  }
+
+  public void setMatchEnd(int matchEnd) {
+    this.matchEnd = matchEnd;
   }
 }
