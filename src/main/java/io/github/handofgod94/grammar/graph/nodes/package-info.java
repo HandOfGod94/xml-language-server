cd4256aa3f3978537114ad1a37825837e32e30e3
Gahan Rakholia <gahan.rakholia@jda.com>
2018-12-22 11:23:39 +0530

    docs: add package-info for new packages

---
+++ src/main/java/io/github/handofgod94/grammar/graph/nodes/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains all the possible Graph nodes(Vertices) for each pattern
+ * described in xml.tmLang.json.
+ */
+package io.github.handofgod94.grammar.graph.nodes;
---
+++ src/main/java/io/github/handofgod94/grammar/graph/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * It comprises of graph creation and context generation
+ * for XML Language. This will then be consumed by graph
+ * processor, to retrieve positional scope information based
+ * on the grammar.
+ */
+package io.github.handofgod94.grammar.graph;
